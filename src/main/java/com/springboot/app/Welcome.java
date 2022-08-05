b1290e62a532202b4cb2e1e19e54d93c0ac236ee
harikrishna8055 <hksagar40@gmail.com>
2022-08-05 14:28:28 +0530

    first commit

---
+++ src/main/java/com/springboot/app/Welcome.java
@@ -0,0 +1,20 @@
+package com.springboot.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	@GetMapping("/")
+	
+	public String students() {
+		return null;
+		
+		
+		
+	}
+
+		
+	}
+
+
